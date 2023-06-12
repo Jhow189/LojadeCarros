3aa3b7bdbaa9b5fe504059b7a0b78444ef9c2e45
Johnny Alves <81985133+Jhow189@users.noreply.github.com>
2023-06-12 11:24:00 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $servidor = "localhost";
-    $usuario = "ead";
-    $senha = "123456789";
-    $banco = "db_ead";
-    $cn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
-    $cn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
